058754c362809b91bd5a1fd4b41f5e1b8c79a8cb
Tiara Lakita <tiaralakita@gmail.com>
2020-05-27 08:44:26 +0800

    Make Dao

---
+++ app/src/main/java/id/ac/unhas/todolistapptiara/data/database/TodoItemDao.kt
@@ -0,0 +1,24 @@
+package id.ac.unhas.todolistapptiara.data.database
+
+import androidx.lifecycle.LiveData
+import androidx.room.*
+
+@Dao
+interface TodoItemDao {
+
+    @Insert
+    suspend fun saveTodoItem(todoItem: TodoItem)
+
+    @Insert
+    suspend fun saveTodoItems(todoItems: List<TodoItem>)
+
+    @Delete
+    suspend fun deleteTodoItem(todoItem: TodoItem)
+
+    @Update
+    suspend fun updateTodoItem(todoItem: TodoItem)
+
+    @Query("SELECT * FROM todo ORDER BY id DESC")
+    fun getAllTodoList(): LiveData<MutableList<TodoItem>>
+
+}
